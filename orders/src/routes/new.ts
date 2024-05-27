11110f3c01d5a0506393e8dcc650120b9c8568c0
sptres <shawnpark2397@gmail.com>
2024-05-27 15:54:52 -0700

    order nats

---
+++ orders/src/routes/new.ts
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+import express, { Request, Response } from 'express';
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@sptrestickets/common';
+import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+
+const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    // find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    // make sure the ticket is available to reserve
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+    // calculate expiration date for order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+    // build the order and save it to database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+    // publish an event saying order is created
+    res.status(201).send(order);
+  }
+);
+
+export { router as newOrderRouter };
